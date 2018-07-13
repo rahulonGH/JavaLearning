8e357ee785477fdb903c14aea15d3d7bbf71a664
Rahul Kumar <39871812+rahulonGH@users.noreply.github.com>
2018-07-13 09:19:59 +0530

    Wrapperclasses

---
+++ 13 July 18/WrapperClassesExample.java
@@ -0,0 +1,14 @@
+package git.Learning;
+
+public class WrapperClassesExample {
+
+	public static void main(String[] args) {
+		
+		int x = 10; //primitive type
+		Integer i = x; // Autoboxing
+		System.out.println(i);
+		int y = i; //Unboxing
+		System.out.println(y);
+	}
+
+}
