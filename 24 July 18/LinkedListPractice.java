bb315746835dd08ecdc7e6b351bcdbed8fbc0503
Rahul Kumar <39871812+rahulonGH@users.noreply.github.com>
2018-07-24 08:42:16 +0530

    LinkedList

---
+++ 24 July 18/LinkedListPractice.java
@@ -0,0 +1,49 @@
+package git.Learning;
+
+import java.util.*;
+
+public class LinkedListPractice {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> llist = new LinkedList<String>();
+		llist.add("RAM");
+		llist.add("SHYAM");
+		llist.add("sonu");
+		llist.add("geeta");
+		llist.add("Seeta");
+		llist.add("sweety");
+		llist.add("titu");
+		llist.add("harpreet");
+		llist.set(0, "RAHUL");
+		llist.removeLast();
+		//System.out.println(llist);
+		llist.addLast("Harpreet");
+		//System.out.println(llist.getLast());
+			Collections.swap(llist, 0, 2);
+		//System.out.println(llist);
+		ListIterator<String> lIt =   llist.listIterator(llist.size());
+		/*while(lIt.hasPrevious())
+		{
+			System.out.println(lIt.previous());				
+		}*/
+		
+		LinkedList<String> llist1 = new LinkedList<String>();
+			llist1.addAll(llist);
+			System.out.println(llist1.size());
+			System.out.println(llist1);
+			llist.set(4, "Deepak");
+			
+			for(String str:llist1){
+				System.out.println(llist.contains(str)?"YES":"NO");
+			}
+			
+			
+		Vector v = new Vector();
+		v.add("RAJU");
+		v.add("X");
+		v.add("Y");
+		System.out.println(v);
+		}
+
+}
